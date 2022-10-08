33bd717db788b5b2898c34d5b41d699ad943dd2c
আলি হাসান <101947194+dev-alihasan@users.noreply.github.com>
2022-10-09 00:10:17 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+          var swiper = new Swiper(".mySwiper", {
+            effect: "flip",
+            grabCursor: !0,
+            pagination: { el: ".swiper-pagination" },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
